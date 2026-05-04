5484e7c7276ecf3aa2f75fbde8bb841694387c4b
Developer <user@example.com>
2026-05-04 20:44:59 +0800

    fix: 在Rust端初始化shell插件，修复删除文件失败问题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             commands::read_directory,
             commands::get_file_info,
